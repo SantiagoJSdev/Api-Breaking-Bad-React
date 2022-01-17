90199500afa1a13d57f0ee803552f57a1e880fad
SantiagoJSdev <santiagoreyesmarin1986@gmail.com>
2022-01-16 19:46:33 -0500

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+Una pequeña aplicación hecha en React con Hooks y componentes, haciendo un llamado a la api https://breakingbadapi.com/
+
+# Recuerden reconstruir los modulos de node
+
+npm install
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RealExampleRef } from './componente/RealExampleRef';
+
+
+
+ReactDOM.render(
+    <RealExampleRef />,
+  document.getElementById('root')
+);
+
